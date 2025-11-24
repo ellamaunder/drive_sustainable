f078331b9ad3ca1aeecba7e60b636b6e4d82a955
Ella Maunder <ellamaunder@Ellas-MacBook-Air-112.local>
2025-11-24 15:52:37 +0000

    dockerfile

--- exercise/api.py
+++ exercise/api.py
@@ -1,28 +1,35 @@
 from flask import Flask, request
 from flask_cors import CORS
+from basket import Basket
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+basket = Basket()
 
-@app.route('/greet/<name>')
-def greet(name):
-    return f'Hello, {name}!'
-
-@app.route('/square', methods=['POST', 'GET'])
-def square():
-    # If a route has multiple methods, you can use if statements to split logic
-    # Or, use multiple functions with the same route but different methods
-    if request.method == 'GET':
-        number = 2
-        return {'result': int(number) ** 2}
+@app.route('/basket', methods=['POST'])
+def add_item():
     data = request.get_json()
-    number = data.get('number', 0)
-    return {'result': int(number) ** 2}
+    name = data.get('name')
+    basket.add_item(name, 1)
+    return { "success": True } 
 
+@app.route('/basket', methods=['DELETE'])
+def remove_item():
+    data = request.get_json()
+    name = data.get('name')
+    basket.remove_item(name, 1)
+    return { "success": True } 
+
+@app.route('/basket', methods=['GET'])
+def view_basket():
+    items = [{'name': item, 'quantity': quantity} for item, quantity in basket.items.items()]
+    return {'items': items}
+
+@app.route('/basket/total', methods=['GET'])
+def get_total_price():
+    price = basket.calculate_price()
+    return {'price': price}
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
